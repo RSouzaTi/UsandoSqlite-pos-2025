008b2e2da0666373c7f57a2b8cfb6bc9ad7600cd
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-01-02 14:53:40 -0300

    Projeto com correções na tela de listar

--- app/src/main/java/br/edu/utfpr/usandosqlite/ListarActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite/ListarActivity.kt
@@ -2,10 +2,7 @@ package br.edu.utfpr.usandosqlite
 
 import android.database.Cursor
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.utfpr.usandosqlite.adapter.MeuAdapter
 import br.edu.utfpr.usandosqlite.database.DatabaseHandler
 import br.edu.utfpr.usandosqlite.databinding.ActivityListarBinding
@@ -14,36 +11,45 @@ class ListarActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityListarBinding
     private lateinit var banco: DatabaseHandler
-
+    private var adapter: MeuAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Temporarily disabled edge-to-edge for diagnostics
+        // enableEdgeToEdge()
 
         binding = ActivityListarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         banco = DatabaseHandler.getInstance(this)
 
+        // Temporarily disabled window insets listener for diagnostics
+        // ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        //     val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //     insets
+        // }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.lvRegistros)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // Set the empty view on the ListView
+        binding.lvRegistros.emptyView = binding.tvEmptyList
 
         initListView()
     }
 
     private fun initListView() {
-
         val cursor: Cursor = banco.listar()
 
-        val adapter = MeuAdapter(this, cursor)
-
-
-      //Chamada do adapter para incluir na interface grafica
-
+        // The adapter will automatically handle showing the empty view
+        // when the cursor is empty.
+        adapter = MeuAdapter(this, cursor)
         binding.lvRegistros.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // The adapter's cursor will be closed automatically by the activity's lifecycle management
+        // when using CursorAdapter.
+        adapter?.cursor?.close()
     }
 }
