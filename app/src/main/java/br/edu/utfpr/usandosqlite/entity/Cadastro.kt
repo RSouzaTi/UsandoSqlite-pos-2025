9adca77fff38298a12c9a82c0e5d1fbf25a77260
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-02-23 12:15:31 -0300

    Projeto pronto, realizado correções.

--- app/src/main/java/br/edu/utfpr/usandosqlite/entity/Cadastro.kt
+++ app/src/main/java/br/edu/utfpr/usandosqlite/entity/Cadastro.kt
@@ -1,7 +1,7 @@
 package br.edu.utfpr.usandosqlite.entity
 
 data class Cadastro(
-    val _id: String,
-    val nome: String,
-    val telefone: String
+    val _id: String = "",
+    val nome: String = "",
+    val telefone: String = "",
 )
